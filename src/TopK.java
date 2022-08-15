50bd946e70961e37a7c7518e9e6c9b9699a16ab5
Samyuktha J <samyuj15@gmail.com>
2022-08-15 22:57:15 +0530

    top k repeating elements in an array

---
+++ src/TopK.java
@@ -0,0 +1,36 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+class TopK {
+    static public int[] topKFrequent(int[] nums, int k) {
+
+        HashMap<Integer, Integer> frequency = new HashMap<>();
+        int res[] = new int[k];
+        for (int number : nums) {
+            if (frequency.containsKey(number)) {
+                frequency.put(number, frequency.get(number) + 1);
+            } else {
+                frequency.put(number, 1);
+            }
+
+        }
+        int i = 0;
+        frequency = frequency.entrySet().stream().sorted((v1, v2) -> -v1.getValue().compareTo(v2.getValue())).limit(k).collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e2, LinkedHashMap::new));
+
+        for (int val : frequency.keySet()) {
+            res[i++] = val;
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+
+        int[] numbers = new int[]{1, 1, 1, 2, 2, 3};
+        int[] res = topKFrequent(numbers, 2);
+        for (int value : res) {
+            System.out.println(value);
+        }
+
+
+    }
+}
