cb2d7fb8327d93fbba00373693642ef169ee7925
Samyuktha <samyuj15@gmail.com>
2023-03-12 10:34:58 +0530

    Valid parantheses

---
+++ src/ValidExpression.java
@@ -0,0 +1,39 @@
+import java.util.Stack;
+
+public class ValidExpression {
+
+    public static void main(String[] args) {
+        String[] inputs = new String[]{"(){}[]", "({}[]", "({}[", "{{}}", "]["};
+        for (String input : inputs) {
+            System.out.println(input + " " + isValidExpression(input));
+        }
+    }
+
+    private static boolean isValidExpression(String input) {
+        Stack<Character> expression = new Stack<>();
+
+        for (char letter : input.toCharArray()) {
+            switch (letter) {
+                case '{':
+                case '[':
+                case '(':
+                    expression.push(letter);
+                    break;
+                case '}':
+                    if (expression.isEmpty() || expression.pop() != '{')
+                        return false;
+                    break;
+                case ')':
+                    if (expression.isEmpty() || expression.pop() != '(')
+                        return false;
+                    break;
+                case ']':
+                    if (expression.isEmpty() || expression.pop() != '[')
+                        return false;
+                    break;
+            }
+
+        }
+        return expression.isEmpty();
+    }
+}
