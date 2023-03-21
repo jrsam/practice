040cbbf3e7a71561cbe69988fd08348b1c128ebd
Samyuktha <samyuj15@gmail.com>
2023-03-21 08:38:13 +0530

    Invert Binary tree-recursion

---
+++ src/InvertBinaryTree.java
@@ -0,0 +1,36 @@
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class InvertBinaryTree {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null)
+            return null;
+        TreeNode val = root.left;
+        root.left = root.right;
+        root.right = val;
+
+        invertTree(root.left);
+        invertTree(root.right);
+        return root;
+
+    }
+
+}
+
