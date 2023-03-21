fb5c516ea512417d4fdbda0e01e11fe745a1c964
Samyuktha <samyuj15@gmail.com>
2023-03-21 11:18:35 +0530

    max depth - recursion

---
+++ src/MaxDepth.java
@@ -0,0 +1,31 @@
+public class MaxDepth {
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public int maxDepth(TreeNode root) {
+        if (root == null)
+            return 0;
+        int left = maxDepth(root.left) + 1;
+        int right = maxDepth(root.right) + 1;
+
+        return Math.max(left, right);
+
+    }
+}
+
